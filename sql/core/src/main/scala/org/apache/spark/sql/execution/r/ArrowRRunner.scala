8954d7285cd251513e1c4e5a827be1fd76eca8d9
Hyukjin Kwon <gurwls223@apache.org>
2019-02-27 14:29:58 +0900

    [SPARK-26830][SQL][R] Vectorized R dapply() implementation
    This PR targets to add vectorized `dapply()` in R, Arrow optimization.
    
    This can be tested as below:
    
    ```bash
    $ ./bin/sparkR --conf spark.sql.execution.arrow.enabled=true
    ```
    
    ```r
    df <- createDataFrame(mtcars)
    collect(dapply(df, function(rdf) { data.frame(rdf$gear + 1) }, structType("gear double")))
    ```
    
      - R 3.5.x
      - Arrow package 0.12+
        ```bash
        Rscript -e 'remotes::install_github("apache/arrowapache-arrow-0.12.0", subdir = "r")'
        ```
    
    **Note:** currently, Arrow R package is not in CRAN. Please take a look at ARROW-3204.
    **Note:** currently, Arrow R package seems not supporting Windows. Please take a look at ARROW-3204.
    
    **Shall**
    
    ```bash
    sync && sudo purge
    ./bin/sparkR --conf spark.sql.execution.arrow.enabled=false --driver-memory 4g
    ```
    
    ```bash
    sync && sudo purge
    ./bin/sparkR --conf spark.sql.execution.arrow.enabled=true --driver-memory 4g
    ```
    
    **R code**
    
    ```r
    rdf <- read.csv("500000.csv")
    df <- cache(createDataFrame(rdf))
    count(df)
    
    test <- function() {
      options(digits.secs = 6) # milliseconds
      start.time <- Sys.time()
      count(cache(dapply(df, function(rdf) { rdf }, schema(df))))
      end.time <- Sys.time()
      time.taken <- end.time - start.time
      print(time.taken)
    }
    
    test()
    ```
    
    **Data (350 MB):**
    
    ```r
    object.size(read.csv("500000.csv"))
    350379504 bytes
    ```
    
    "500000 Records"  http://eforexcel.com/wp/downloads-16-sample-csv-files-data-sets-for-testing/
    
    **Results**
    
    ```
    Time difference of 13.42037 mins
    ```
    
    ```
    Time difference of 30.64156 secs
    ```
    
    The performance improvement was around **2627%**.
    
    - For now, Arrow optimization with R does not support when the data is `raw`, and when user explicitly gives float type in the schema. They produce corrupt values.
    
    - Due to ARROW-4512, it cannot send and receive batch by batch. It has to send all batches in Arrow stream format at once. It needs improvement later.
    
    Unit tests were added, and manually tested.
    
    Closes #23787 from HyukjinKwon/SPARK-26830-1.
    
    Authored-by: Hyukjin Kwon <gurwls223@apache.org>
    Signed-off-by: Hyukjin Kwon <gurwls223@apache.org>

--- sql/core/src/main/scala/org/apache/spark/sql/execution/r/ArrowRRunner.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/r/ArrowRRunner.scala
@@ -47,6 +47,7 @@ class ArrowRRunner(
     broadcastVars: Array[Broadcast[Object]],
     schema: StructType,
     timeZoneId: String,
+    mode: Int,
     condaSetupInstructions: Option[CondaSetupInstructions])
   extends RRunner[ColumnarBatch](
     func,
@@ -58,13 +59,32 @@ class ArrowRRunner(
     numPartitions = -1,
     isDataFrame = true,
     schema.fieldNames,
-    RRunnerModes.DATAFRAME_GAPPLY) {
+    mode) {
+
+  // TODO: it needs to refactor to share the same code with RRunner, and have separate
+  // ArrowRRunners.
+  private val getNextBatch = {
+    if (mode == RRunnerModes.DATAFRAME_GAPPLY) {
+      // gapply
+      (inputIterator: Iterator[_], keys: collection.mutable.ArrayBuffer[Array[Byte]]) => {
+        val (key, nextBatch) = inputIterator
+          .asInstanceOf[Iterator[(Array[Byte], Iterator[InternalRow])]].next()
+        keys.append(key)
+        nextBatch
+      }
+    } else {
+      // dapply
+      (inputIterator: Iterator[_], keys: collection.mutable.ArrayBuffer[Array[Byte]]) => {
+        inputIterator
+          .asInstanceOf[Iterator[Iterator[InternalRow]]].next()
+      }
+    }
+  }
 
   protected override def writeData(
       dataOut: DataOutputStream,
       printOut: PrintStream,
-      iter: Iterator[_]): Unit = if (iter.hasNext) {
-    val inputIterator = iter.asInstanceOf[Iterator[(Array[Byte], Iterator[InternalRow])]]
+      inputIterator: Iterator[_]): Unit = if (inputIterator.hasNext) {
     val arrowSchema = ArrowUtils.toArrowSchema(schema, timeZoneId)
     val allocator = ArrowUtils.rootAllocator.newChildAllocator(
       "stdout writer for R", 0, Long.MaxValue)
@@ -78,8 +98,7 @@ class ArrowRRunner(
       writer.start()
 
       while (inputIterator.hasNext) {
-        val (key, nextBatch) = inputIterator.next()
-        keys.append(key)
+        val nextBatch: Iterator[InternalRow] = getNextBatch(inputIterator, keys)
 
         while (nextBatch.hasNext) {
           arrowWriter.write(nextBatch.next())
