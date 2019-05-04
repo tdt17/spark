7999d1c2daa27788fe2e91c13bf2f92aa7c28f14
Liang-Chi Hsieh <viirya@gmail.com>
2019-05-04 13:21:08 +0900

    [SPARK-27629][PYSPARK] Prevent Unpickler from intervening each unpickling
    In SPARK-27612, one correctness issue was reported. When protocol 4 is used to pickle Python objects, we found that unpickled objects were wrong. A temporary fix was proposed by not using highest protocol.
    
    It was found that Opcodes.MEMOIZE was appeared in the opcodes in protocol 4. It is suspect to this issue.
    
    A deeper dive found that Opcodes.MEMOIZE stores objects into internal map of Unpickler object. We use single Unpickler object to unpickle serialized Python bytes. Stored objects intervenes next round of unpickling, if the map is not cleared.
    
    We has two options:
    
    1. Continues to reuse Unpickler, but calls its close after each unpickling.
    2. Not to reuse Unpickler and create new Unpickler object in each unpickling.
    
    This patch takes option 1.
    
    Passing the test added in SPARK-27612 (#24519).
    
    Closes #24521 from viirya/SPARK-27629.
    
    Authored-by: Liang-Chi Hsieh <viirya@gmail.com>
    Signed-off-by: HyukjinKwon <gurwls223@apache.org>

--- python/pyspark/serializers.py
+++ python/pyspark/serializers.py
@@ -61,13 +61,12 @@ import itertools
 
 if sys.version < '3':
     import cPickle as pickle
-    protocol = 2
     from itertools import izip as zip, imap as map
 else:
     import pickle
-    protocol = 3
     basestring = unicode = str
     xrange = range
+pickle_protocol = pickle.HIGHEST_PROTOCOL
 
 from pyspark import cloudpickle
 from pyspark.util import _exception_message
@@ -671,7 +670,7 @@ class PickleSerializer(FramedSerializer):
     """
 
     def dumps(self, obj):
-        return pickle.dumps(obj, protocol)
+        return pickle.dumps(obj, pickle_protocol)
 
     if sys.version >= '3':
         def loads(self, obj, encoding="bytes"):
@@ -685,7 +684,7 @@ class CloudPickleSerializer(PickleSerializer):
 
     def dumps(self, obj):
         try:
-            return cloudpickle.dumps(obj, 2)
+            return cloudpickle.dumps(obj, pickle_protocol)
         except pickle.PickleError:
             raise
         except Exception as e:
