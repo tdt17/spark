db50a6dec05060924a2752249ee0a7425b4e461a
Josh Casale <jcasale@palantir.com>
2020-08-10 18:18:10 +0100

    nit in PythonRunner

--- core/src/main/scala/org/apache/spark/api/python/PythonRunner.scala
+++ core/src/main/scala/org/apache/spark/api/python/PythonRunner.scala
@@ -91,8 +91,9 @@ private[spark] abstract class BasePythonRunner[IN, OUT](
 
   // All the Python functions should have the same exec, version and envvars.
   protected val envVars: java.util.Map[String, String] = funcs.head.funcs.head.envVars
-  protected val pythonExec: String = funcs.head.funcs.head.pythonExec
+  protected val pythonExec = funcs.head.funcs.head.pythonExec
   protected val pythonVer: String = funcs.head.funcs.head.pythonVer
+  protected val condaInstructions = funcs.head.funcs.head.condaSetupInstructions
 
   // TODO: support accumulator in multiple UDF
   protected val accumulator: PythonAccumulatorV2 = funcs.head.funcs.head.accumulator
