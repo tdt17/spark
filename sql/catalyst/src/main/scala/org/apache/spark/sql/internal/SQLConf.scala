19ade716dec7e3e810da9a2e8fafc787a35946bd
Will Raschkowski <wraschkowski@palantir.com>
2020-09-22 18:13:02 +0200

    Try fixing VersionsSuite by adding Palantir bintray to additional remote repos

--- sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
@@ -2444,7 +2444,8 @@ object SQLConf {
       .version("3.0.0")
       .stringConf
       .createWithDefault(
-        "https://maven-central.storage-download.googleapis.com/maven2/")
+        "https://maven-central.storage-download.googleapis.com/maven2/,"
+      + "http://dl.bintray.com/palantir/releases")
 
   val LEGACY_FROM_DAYTIME_STRING =
     buildConf("spark.sql.legacy.fromDayTimeString.enabled")
