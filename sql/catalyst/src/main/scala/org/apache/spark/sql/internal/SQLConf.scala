20dc34a1a67c01b7cbfd433deff681b891cd1379
Will Raschkowski <wraschkowski@palantir.com>
2020-12-19 20:11:22 +0100

    Try removing bintray from additional repositories config

--- sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
@@ -2453,8 +2453,7 @@ object SQLConf {
       .version("3.0.0")
       .stringConf
       .createWithDefault(
-        "https://maven-central.storage-download.googleapis.com/maven2/,"
-      + "http://dl.bintray.com/palantir/releases")
+        "https://maven-central.storage-download.googleapis.com/maven2/")
 
   val LEGACY_FROM_DAYTIME_STRING =
     buildConf("spark.sql.legacy.fromDayTimeString.enabled")
