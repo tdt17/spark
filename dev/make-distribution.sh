5ede7ff5a29629257b83d9044f97482a0c25e3a6
Will Raschkowski <wraschkowski@palantir.com>
2021-01-04 15:26:46 +0100

    undo selective cleaning optimization in dev/make-distribution.sh

--- dev/make-distribution.sh
+++ dev/make-distribution.sh
@@ -35,7 +35,6 @@ DISTDIR="$SPARK_HOME/dist"
 MAKE_TGZ=false
 MAKE_PIP=false
 MAKE_R=false
-CLEAN=false
 NAME=none
 MVN="$SPARK_HOME/build/mvn"
 
@@ -44,7 +43,7 @@ function exit_with_usage {
   echo "make-distribution.sh - tool for making binary distributions of Spark"
   echo ""
   echo "usage:"
-  cl_options="[--name] [--tgz] [--pip] [--r] [--clean] [--mvn <mvn-command>]"
+  cl_options="[--name] [--tgz] [--pip] [--r] [--mvn <mvn-command>]"
   echo "make-distribution.sh $cl_options <maven build options>"
   echo "See Spark's \"Building Spark\" doc for correct Maven options."
   echo ""
@@ -62,9 +61,6 @@ while (( "$#" )); do
       ;;
     --r)
       MAKE_R=true
-      ;;
-    --clean)
-      CLEAN=true
       ;;
     --mvn)
       MVN="$2"
@@ -164,26 +160,16 @@ cd "$SPARK_HOME"
 
 export MAVEN_OPTS="${MAVEN_OPTS:--Xmx2g -XX:ReservedCodeCacheSize=1g}"
 
-if [[ $CLEAN == true ]]; then
-  MAYBE_CLEAN="clean"
-else
-  MAYBE_CLEAN=""
-fi
-
 # Store the command as an array because $MVN variable might have spaces in it.
 # Normal quoting tricks don't work.
 # See: http://mywiki.wooledge.org/BashFAQ/050
-if [[ -z "$DONT_BUILD" ]]; then
-  BUILD_COMMAND=("$MVN" $MAYBE_CLEAN package -DskipTests $@)
+BUILD_COMMAND=("$MVN" clean package -DskipTests $@)
 
 # Actually build the jar
 echo -e "\nBuilding with..."
 echo -e "\$ ${BUILD_COMMAND[@]}\n"
 
-  "${BUILD_COMMAND[@]}"
-else
-  echo -e "\nNot running mvn package because \$DONT_BUILD was set"
-fi
+"${BUILD_COMMAND[@]}"
 
 # Make directories
 rm -rf "$DISTDIR"
