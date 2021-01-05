27983b912c49678768a8cfaa4ab0d6d12f736250
Will Raschkowski <wraschkowski@palantir.com>
2021-01-05 13:11:38 +0100

    No longer set DONT_BUILD after removing optimization

--- dev/publish-local.sh
+++ dev/publish-local.sh
@@ -8,6 +8,6 @@ MVN_LOCAL=~/.m2/repository
 source "$FWDIR/publish_functions.sh"
 
 set_version_and_install
-DONT_BUILD=true make_dist
+make_dist
 mkdir -p $MVN_LOCAL/org/apache/spark/${artifact_name}/${version}
 cp $file_name $MVN_LOCAL/org/apache/spark/${artifact_name}/${version}/${artifact_name}-${version}.tgz
