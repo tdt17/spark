94c114031cbb17914481944853d4bc04ab802897
Yuming Wang <yumwang@ebay.com>
2021-01-17 21:54:35 -0800

    [SPARK-33696][BUILD][SQL] Upgrade built-in Hive to 2.3.8
    Hive 2.3.8 changes:
    HIVE-19662: Upgrade Avro to 1.8.2
    HIVE-24324: Remove deprecated API usage from Avro
    HIVE-23980: Shade Guava from hive-exec in Hive 2.3
    HIVE-24436: Fix Avro NULL_DEFAULT_VALUE compatibility issue
    HIVE-24512: Exclude calcite in packaging.
    HIVE-22708: Fix for HttpTransport to replace String.equals
    HIVE-24551: Hive should include transitive dependencies from calcite after shading it
    HIVE-24553: Exclude calcite from test-jar dependency of hive-exec
    
    Upgrade Avro and Parquet to latest version.
    
    No.
    
    Existing test add test try to upgrade Parquet to 1.11.1 and Avro to 1.10.1: https://github.com/apache/spark/pull/30517
    
    Closes #30657 from wangyum/SPARK-33696.
    
    Authored-by: Yuming Wang <yumwang@ebay.com>
    Signed-off-by: Dongjoon Hyun <dhyun@apple.com>

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/client/package.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/client/package.scala
@@ -87,11 +87,13 @@ package object client {
         "org.apache.curator:*",
         "org.pentaho:pentaho-aggdesigner-algorithm"))
 
-    // Since HIVE-14496, Hive materialized view need calcite-core.
+    // Since HIVE-23980, calcite-core included in Hive package jar.
     // For spark, only VersionsSuite currently creates a hive materialized view for testing.
-    case object v2_3 extends HiveVersion("2.3.7",
-      exclusions = Seq("org.apache.calcite:calcite-druid",
+    case object v2_3 extends HiveVersion("2.3.8",
+      exclusions = Seq("org.apache.calcite:calcite-core",
+        "org.apache.calcite:calcite-druid",
         "org.apache.calcite.avatica:avatica",
+        "com.fasterxml.jackson.core:*",
         "org.apache.curator:*",
         "org.pentaho:pentaho-aggdesigner-algorithm"))
 
@@ -101,7 +103,6 @@ package object client {
       extraDeps = Seq("org.apache.logging.log4j:log4j-api:2.10.0",
         "org.apache.derby:derby:10.14.1.0"),
       exclusions = Seq("org.apache.calcite:calcite-druid",
-        "org.apache.calcite.avatica:avatica",
         "org.apache.curator:*",
         "org.pentaho:pentaho-aggdesigner-algorithm"))
 
@@ -111,7 +112,6 @@ package object client {
       extraDeps = Seq("org.apache.logging.log4j:log4j-api:2.10.0",
         "org.apache.derby:derby:10.14.1.0"),
       exclusions = Seq("org.apache.calcite:calcite-druid",
-        "org.apache.calcite.avatica:avatica",
         "org.apache.curator:*",
         "org.pentaho:pentaho-aggdesigner-algorithm"))
 
